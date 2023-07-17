383e0ccbdf94dab9383a9dd8046907448a2c461a
Jesse Williams <jesse@negativespacesounds.com>
2023-07-17 22:45:30 +1000

    feat: basic contact form design

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,5 @@
 <div class="w-full dark:bg-stone-900 flex justify-evenly">
+    <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/">Home</a>
     <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/background">Background</a>
     <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/projects">Projects</a>
     <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/contact">Contact</a>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	let name = '';
+	let email= '';
+	let message = '';
+
+	/**
+	 * @param {Event & { currentTarget: HTMLInputElement }} event
+	 */
+	function setName(event) {
+		name = event.currentTarget.value;
+	}
+
+	/**
+	 * @param {Event & { currentTarget: HTMLInputElement }} event
+	 */
+	function setEmail(event) {
+		email = event.currentTarget.value;
+	}
+
+	/**
+	 * @param {Event & { currentTarget: HTMLTextAreaElement }} event
+	 */
+	function setMessage(event) {
+		message = event.currentTarget.value;
+	}
+</script>
+
+<div class="flex justify-evenly pt-32">
+	<form class="flex flex-col w-72 dark:border-stone-200 h-96 justify-evenly">
+        <label for="name">Name</label>
+		<input id="name" class="dark:bg-stone-950 dark:border-stone-200 border-2 focus:shadow-glow" type="text" on:change={setName} />
+        <label for="email">Email</label>
+		<input id="email" class="dark:bg-stone-950 dark:border-stone-200 border-2 focus:shadow-glow" type="text" on:change={setEmail} />
+        <label for="message">Message</label>
+        <textarea id="message" class="dark:bg-stone-950 dark:border-stone-200 border-2 focus:shadow-glow" on:change={setMessage} />
+		<button class="hover:dark:bg-stone-600 p-4" on:click={() => console.log(name, email, message)}>Submit</button>
+	</form>
+</div>
