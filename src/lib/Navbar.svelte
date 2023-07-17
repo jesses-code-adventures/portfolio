2a81c6fc280713f72b9b070b7d227276bcf861e2
Jesse Williams <jesse@negativespacesounds.com>
2023-07-18 01:54:31 +1000

    feat: footer

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,6 +1,6 @@
-<div class="w-full dark:bg-stone-900 flex justify-evenly">
-    <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/">Home</a>
-    <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/background">Background</a>
-    <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/projects">Projects</a>
-    <a class="text-white text-xl p-4 dark:hover:bg-stone-700" href="/contact">Contact</a>
+<div class="w-full dark:bg-stone-900 flex justify-evenly px-16 md:px-0">
+    <a class="text-white sm:text-xl p-4 dark:hover:bg-stone-700" href="/">Home</a>
+    <a class="text-white sm:text-xl p-4 dark:hover:bg-stone-700" href="/background">Background</a>
+    <a class="text-white sm:text-xl p-4 dark:hover:bg-stone-700" href="/projects">Projects</a>
+    <a class="text-white sm:text-xl p-4 dark:hover:bg-stone-700" href="/contact">Contact</a>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import '../app.css';
     import Navbar from "$lib/Navbar.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
 
-<div class="sm:h-screen dark:bg-stone-950 bg-white dark:text-white text-black">
+<div class="sm:h-full dark:bg-stone-950 bg-white dark:text-white text-black">
     <Navbar />
     <slot />
     <div class="h-16" />
+    <Footer />
 </div>
