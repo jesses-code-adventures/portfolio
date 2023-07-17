850534f035cdd0b9639248ccada16573aecc9836
Jesse Williams <jesse@negativespacesounds.com>
2023-07-18 02:18:46 +1000

    update: prerender errythang

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,24 +1,49 @@
 <script>
-	import Image from "./Image.svelte";
-
+	import Image from './Image.svelte';
 </script>
+
 <div class="w-full dark:bg-stone-950 bg-white dark:text-white text-black flex justify-evenly py-8">
-    <a target="_blank" href="https://soundcloud.com/jessesbeetshoppe/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="soundcloud.webp" alt="soundcloud" tailwindClass="w-6 h-6" />
-    </a>
-    <a target="_blank" href="https://twitter.com/jessesbeets/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="twitter.webp" alt="twitter" tailwindClass="w-6 h-6" />
-    </a>
-    <a target="_blank" href="https://www.linkedin.com/in/jesse-williams-16b70bba/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="linkedin.webp" alt="linkedin" tailwindClass="w-6 h-6" />
-    </a>
-    <a target="_blank" href="https://github.com/jesses-code-adventures/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="github.webp" alt="github" tailwindClass="w-6 h-6" />
-    </a>
-    <a target="_blank" rel="noopener noreferrer" href="mailto:jesse@negativespacesounds.com?subject=Website%20Contact&body=Hello!" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="gmail.webp" alt="email" tailwindClass="w-6 h-6" />
-    </a>
-    <a target="_blank" href="https://open.spotify.com/playlist/08MtGBJVaGsawTXaiC3M9m?si=9df6230465f34073" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="spotify.webp" alt="spotify" tailwindClass="w-6 h-6" />
-    </a>
+	<a
+		target="_blank"
+		href="https://soundcloud.com/jessesbeetshoppe/"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="soundcloud.webp" alt="soundcloud" tailwindClass="w-6 h-6" />
+	</a>
+	<a
+		target="_blank"
+		href="https://twitter.com/jessesbeets/"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="twitter.webp" alt="twitter" tailwindClass="w-6 h-6" />
+	</a>
+	<a
+		target="_blank"
+		href="https://www.linkedin.com/in/jesse-williams-16b70bba/"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="linkedin.webp" alt="linkedin" tailwindClass="w-6 h-6" />
+	</a>
+	<a
+		target="_blank"
+		href="https://github.com/jesses-code-adventures/"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="github.webp" alt="github" tailwindClass="w-6 h-6" />
+	</a>
+	<a
+		target="_blank"
+		rel="noopener noreferrer"
+		href="mailto:jesse@negativespacesounds.com?subject=Website%20Contact&body=Hello!"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="gmail.webp" alt="email" tailwindClass="w-6 h-6" />
+	</a>
+	<a
+		target="_blank"
+		href="https://open.spotify.com/playlist/08MtGBJVaGsawTXaiC3M9m?si=9df6230465f34073"
+		class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm"
+	>
+		<Image src="spotify.webp" alt="spotify" tailwindClass="w-6 h-6" />
+	</a>
 </div>
