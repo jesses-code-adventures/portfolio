a4e01d9f5225664d17e80b8cc1acce68fe3334b8
Jesse Williams <jesse@negativespacesounds.com>
2023-07-18 02:03:28 +1000

    update: github

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,11 +6,14 @@
     <a target="_blank" href="https://soundcloud.com/jessesbeetshoppe/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
         <Image src="soundcloud.webp" alt="soundcloud" tailwindClass="w-6 h-6" />
     </a>
+    <a target="_blank" href="https://twitter.com/jessesbeets/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
+        <Image src="twitter.webp" alt="twitter" tailwindClass="w-6 h-6" />
+    </a>
     <a target="_blank" href="https://www.linkedin.com/in/jesse-williams-16b70bba/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
         <Image src="linkedin.webp" alt="linkedin" tailwindClass="w-6 h-6" />
     </a>
-    <a target="_blank" href="https://twitter.com/jessesbeets/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
-        <Image src="twitter.webp" alt="twitter" tailwindClass="w-6 h-6" />
+    <a target="_blank" href="https://github.com/jesses-code-adventures/" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
+        <Image src="github.webp" alt="github" tailwindClass="w-6 h-6" />
     </a>
     <a target="_blank" rel="noopener noreferrer" href="mailto:jesse@negativespacesounds.com?subject=Website%20Contact&body=Hello!" class="p-4 hover:dark:bg-stone-600 hover:bg-stone-300 rounded-sm">
         <Image src="gmail.webp" alt="email" tailwindClass="w-6 h-6" />
